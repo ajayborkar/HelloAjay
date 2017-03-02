c63387fb3726d1060593887fd625af6f61c57767
Ajay Borkar <a.s.borkar@gmail.com>
2017-03-03 02:27:38 +0530

    GridView Program

---
+++ WebApplication2/Default.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head id="Head1" runat="server">
+    <title>Untitled Page</title>
+    <style type="text/css">
+.Gridview
+{
+font-family:Verdana;
+font-size:10pt;
+font-weight:normal;
+color:black;
+ 
+}
+</style>
+<script type="text/javascript">
+ 
+</script>
+</head>
+<body>
+    <form id="form1" runat="server">
+<div>
+
+    <br />
+    <br />
+    <asp:TextBox ID="TextBox11" runat="server"></asp:TextBox>
+    <br />
+    <asp:TextBox ID="TextBox22" runat="server"></asp:TextBox>
+    <br />
+    <asp:TextBox ID="TextBox33" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button ID="Button1" runat="server" Text="Update" OnClick="Button1_Click" />
+    <br />
+    <br />
+    <br />
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="id" OnPageIndexChanging="GridView1_PageIndexChanging" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowDeleting="GridView1_RowDeleting" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" OnSelectedIndexChanging="GridView1_SelectedIndexChanging" CellPadding="4" ForeColor="#333333" GridLines="None">
+        <AlternatingRowStyle BackColor="White" />
+    <Columns>
+        <asp:TemplateField HeaderText="S.No.">
+            <EditItemTemplate>
+                <asp:TextBox ID="txtID" runat="server" Text='<%# Bind("id") %>'></asp:TextBox>
+            </EditItemTemplate>
+            <ItemTemplate>
+                <asp:Label ID="lblID" runat="server" Text='<%# Bind("id") %>'></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="Name">
+            <EditItemTemplate>
+                <asp:TextBox ID="txtName" runat="server" Text='<%# Bind("name") %>'></asp:TextBox>
+            </EditItemTemplate>
+            <ItemTemplate>
+                <asp:Label ID="lblName" runat="server" Text='<%# Bind("name") %>'></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="address">
+            <EditItemTemplate>
+                <asp:TextBox ID="txtaddress" runat="server" Text='<%# Bind("address") %>'></asp:TextBox>
+            </EditItemTemplate>
+            <ItemTemplate>
+                <asp:Label ID="lbladdress" runat="server" Text='<%# Bind("address") %>'></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="Country">
+            <EditItemTemplate>
+                <asp:TextBox ID="txtCountry" runat="server" Text='<%# Bind("country") %>'></asp:TextBox>
+            </EditItemTemplate>
+            <ItemTemplate>
+                <asp:Label ID="lblCountry" runat="server" Text='<%# Bind("country") %>'></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:CommandField ShowDeleteButton="true" />
+        <asp:CommandField ShowSelectButton="True" />
+        <asp:CommandField ShowEditButton="True" />
+        </Columns>
+        <EditRowStyle BackColor="#2461BF" />
+        <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#EFF3FB" />
+        <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#F5F7FB" />
+        <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+        <SortedDescendingCellStyle BackColor="#E9EBEF" />
+        <SortedDescendingHeaderStyle BackColor="#4870BE" />
+    </asp:GridView>
+ 
+    </div>
+<div>
+<asp:Label ID="lblresult" runat="server"></asp:Label>
+    <br />
+    
+</div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        private SqlConnection conn = new SqlConnection("Data Source=ajay-pc;Integrated Security=true;Initial Catalog=test1");
+        int updateID;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                gvbind();
+            }
+
+        }
+
+        protected void gvbind()
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * from tbl1", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            conn.Close();
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+            else
+            {
+                ds.Tables[0].Rows.Add(ds.Tables[0].NewRow());
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+                int columncount = GridView1.Rows[0].Cells.Count;
+                GridView1.Rows[0].Cells.Clear();
+                GridView1.Rows[0].Cells.Add(new TableCell());
+                GridView1.Rows[0].Cells[0].ColumnSpan = columncount;
+                GridView1.Rows[0].Cells[0].Text = "No Records Found";
+            }
+
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            GridViewRow row = (GridViewRow)GridView1.Rows[e.RowIndex];
+            Label lbldeleteid = (Label)row.FindControl("lblID");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("delete FROM tbl1 where id='" + Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString()) + "'", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            gvbind();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+            GridView1.EditIndex = e.NewEditIndex;
+            gvbind();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            gvbind();
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            gvbind();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int userid = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+            GridViewRow row = (GridViewRow)GridView1.Rows[e.RowIndex];
+            Label lblID = (Label)row.FindControl("lblID");
+            //TextBox txtname=(TextBox)gr.cell[].control[];
+            TextBox textName = (TextBox)row.FindControl("txtName");
+            TextBox textadd = (TextBox)row.FindControl("txtAddress");
+            TextBox textc = (TextBox)row.FindControl("txtCountry");
+            //TextBox textadd = (TextBox)row.FindControl("txtadd");
+            //TextBox textc = (TextBox)row.FindControl("txtc");
+            GridView1.EditIndex = -1;
+            conn.Open();
+            //SqlCommand cmd = new SqlCommand("SELECT * FROM detail", conn);
+            SqlCommand cmd = new SqlCommand("update tbl1 set name='" + textName.Text + "',address='" + textadd.Text + "',country='" + textc.Text + "'where id='" + userid + "'", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            gvbind();
+            //GridView1.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //int userid = Convert.ToInt32(GridView1.DataKeys[e.].Value.ToString());
+        }
+
+        protected void GridView1_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+        {
+            int userid = Convert.ToInt32(GridView1.DataKeys[e.NewSelectedIndex ].Value.ToString());
+            GridViewRow row = (GridViewRow)GridView1.Rows[e.NewSelectedIndex];
+
+            Label textName = (Label )row.FindControl("lblName");
+            Label textadd = (Label)row.FindControl("lblAddress");
+            Label textc = (Label)row.FindControl("lblCountry");
+
+            TextBox11.Text = textName.Text;
+            updateID = userid;
+            TextBox22.Text = textadd.Text;
+            TextBox33.Text = textc.Text;
+
+            TextBox11.Focus();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            //SqlCommand cmd = new SqlCommand("SELECT * FROM detail", conn);
+            SqlCommand cmd = new SqlCommand("update tbl1 set name='" + TextBox11.Text + "',address='" + TextBox22.Text + "',country='"
+                + TextBox33.Text + "'where id='" + updateID + "'", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            TextBox11.Text = "";
+            updateID = 0;
+            TextBox22.Text = "";
+            TextBox33.Text = "";
+
+            GridView1.SelectedIndex = -1;
+
+            gvbind();
+        }
+    }
+}
